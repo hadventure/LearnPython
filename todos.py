a69133b0b04a4cf4441c0da88dce59b6d8a9b98c
hadventure <halikhacheva@gmail.com>
2024-08-22 18:18:32 +0300

    feat(day 6): todod via txt file

--- todos.py
+++ todos.py
@@ -1,20 +1,29 @@
-user_prompt = "Please enter a todo: "
-
-todos = []
+user_prompt = f"Please enter a todo: \n"
 
 while True:
-    user_action = input("Type add, show, edit or exit")
+    user_action = input("Type add, show, edit or exit \n")
     user_action = user_action.strip()
 
     match user_action:
         case "add":
             todo = input(user_prompt)
-            todos.append(todo)
+
+            file = open("todos.txt", "a+")
+            todos = file.readlines()
+            file.close()
+
+            todos.append(f"{todo}\n")
+
+            file = open("todos.txt", "a+")
+            file.writelines(todos)
+            file.close()
         case "show" | "display":
+            file = open("todos.txt", "r")
+            todos = file.readlines()
+
             print(todos)
-            for index, todo in enumerate(todos):
-                row = f"{index}-item"
-                print(index, "-", todo)
+
+            file.close()
         case "edit":
             number = int(input("Enter todos number"))
             number = number - 1
