66973daaf8362789f1714edd28afbddb302b325c
hadventure <halikhacheva@gmail.com>
2024-08-27 16:45:35 +0300

    feat(day 11): add work with file system

--- todos.py
+++ todos.py
@@ -8,9 +8,8 @@ while True:
         case "add":
             todo = input(user_prompt)
 
-            file = open("todos.txt", "a+")
-            todos = file.readlines()
-            file.close()
+            with open("todos.txt", "a+") as file:
+                todos = file.readlines()
 
             todos.append(f"{todo}\n")
 
@@ -22,16 +21,43 @@ while True:
             todos = file.readlines()
             file.close()
 
+            new_todos = []
+            new_todos = [item.strip("\n") for item in new_todos]
+
             for index, todo in enumerate(todos):
                 print(f"{index}. {todo}")
         case "edit":
             number = int(input("Enter todos number"))
             number = number - 1
-            new_todo = input("Enter updated todo")
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+
+            print(todos)
+
+            new_todo = input("Enter updated todo" ) + "\n"
+
             todos[number] = new_todo
+
+            print(todos)
+
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
         case "complete":
             number = int(input("Enter completed number"))
-            todos.pop(number - 1)
+
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
+
+            index = number - 1
+            todo_to_remove = todos[index]
+            todos.pop(index)
+
+            with open("todos.txt", "w") as file:
+                file.writelines(todos)
+
+            message = f"Todo {todo_to_remove} was removed"
+
+
         case "exit":
             break
         case _:
