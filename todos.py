d8c6da13653409805321e7d3344d64ea45011f8d
hadventure <halikhacheva@gmail.com>
2024-08-28 17:38:40 +0300

    feat(day 12): using try except

--- todos.py
+++ todos.py
@@ -4,60 +4,67 @@ while True:
     user_action = input("Type add, show, edit or exit \n")
     user_action = user_action.strip()
 
-    if "add" in user_action:
-            todo = input(user_prompt)
+    if user_action.startswith("add"):
+        todo = input(user_prompt)
 
-            with open("todos.txt", "a+") as file:
+        with open("todos.txt", "a+") as file:
+            todos = file.readlines()
+
+        todos.append(f"{todo}\n")
+
+        file = open("todos.txt", "a+")
+        file.writelines(todos)
+        file.close()
+    elif user_action.startswith("show"):
+        file = open("todos.txt", "r")
+        todos = file.readlines()
+        file.close()
+
+        new_todos = []
+        new_todos = [item.strip("\n") for item in new_todos]
+
+        for index, todo in enumerate(todos):
+            print(f"{index}. {todo}")
+    elif user_action.startswith("edit"):
+        try:
+            number = int(input("Enter todos number"))
+            number = number - 1
+            with open('todos.txt', 'r') as file:
                 todos = file.readlines()
 
-            todos.append(f"{todo}\n")
+            print(todos)
 
-            file = open("todos.txt", "a+")
-            file.writelines(todos)
-            file.close()
-    elif "show" | "display" in user_action:
-            file = open("todos.txt", "r")
-            todos = file.readlines()
-            file.close()
+            new_todo = input("Enter updated todo") + "\n"
 
-            new_todos = []
-            new_todos = [item.strip("\n") for item in new_todos]
+            todos[number] = new_todo
 
-            for index, todo in enumerate(todos):
-                print(f"{index}. {todo}")
-    elif "edit" in user_action:
-        number = int(input("Enter todos number"))
-        number = number - 1
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
+            print(todos)
 
-        print(todos)
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+        except ValueError:
+            print("Command is not valid")
+            user_action = input("Type add, show, edit or exit \n")
+            user_action = user_action.strip()
+    elif user_action.startswith("complete"):
+        try:
+            number = int(input("Enter completed number"))
 
-        new_todo = input("Enter updated todo" ) + "\n"
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
 
-        todos[number] = new_todo
+            index = number - 1
+            todo_to_remove = todos[index]
+            todos.pop(index)
 
-        print(todos)
+            with open("todos.txt", "w") as file:
+                file.writelines(todos)
 
-        with open('todos.txt', 'w') as file:
-            file.writelines(todos)
-
-    elif "complete" in user_action:
-        number = int(input("Enter completed number"))
-
-        with open("todos.txt", "r") as file:
-            todos = file.readlines()
-
-        index = number - 1
-        todo_to_remove = todos[index]
-        todos.pop(index)
-
-        with open("todos.txt", "w") as file:
-            file.writelines(todos)
-
-        message = f"Todo {todo_to_remove} was removed"
-
-    elif "exit" in user_action:
+            message = f"Todo {todo_to_remove} was removed"
+        except IndexError:
+            print("There is no item with that index")
+            continue
+    elif user_action.startswith("exit"):
         break
     else:
         print("Invalid input")
