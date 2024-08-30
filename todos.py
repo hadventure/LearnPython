8c06d9e4a28d4669a1b770e2be99e0cce7af5754
hadventure <halikhacheva@gmail.com>
2024-08-30 12:22:08 +0300

    feat(day 14): add default arguments, docstrings

--- todos.py
+++ todos.py
@@ -1,14 +1,23 @@
 user_prompt = f"Please enter a todo: \n"
 
 
-def get_todos(filepath):
+def get_todos(filepath="todos.txt"):
+    """
+    Read a text file and return the list of.
+    """
     with open(filepath, "a+") as file_local:
         file_local.seek(0)
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(filepath, todos_arg):
+def write_todos(todos_arg, filepath="todos.txt"):
+    """
+    Read a text file and return the list of.
+    :param todos_arg:
+    :param filepath:
+    :return:
+    """
     with open(filepath, "w") as file:
         file.writelines(todos_arg)
 
@@ -20,10 +29,10 @@ while True:
     if user_action.startswith("add"):
         todo = input(user_prompt)
 
-        todos = get_todos("todos.txt")
+        todos = get_todos()
         todos.append(f"{todo}\n")
 
-        write_todos("todos.txt", todos)
+        write_todos(todos_arg=todos)
     elif user_action.startswith("show"):
         todos = get_todos("todos.txt")
 
@@ -43,7 +52,7 @@ while True:
 
             todos[number] = new_todo
 
-            write_todos("todos.txt", todos)
+            write_todos(todos)
         except ValueError:
             print("Command is not valid")
             user_action = input("Type add, show, edit or exit \n")
@@ -52,13 +61,13 @@ while True:
         try:
             number = int(input("Enter completed number"))
 
-            todos = get_todos("todos.txt")
+            todos = get_todos()
 
             index = number - 1
             todo_to_remove = todos[index]
             todos.pop(index)
 
-            write_todos("todos.txt", todos)
+            write_todos(todos)
 
             message = f"Todo {todo_to_remove} was removed"
         except IndexError:
