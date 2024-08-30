17b1fc6254c84b8bb6f9ea74fdc3cb87b3c5a9c1
hadventure <halikhacheva@gmail.com>
2024-08-30 22:23:19 +0300

    feat(day 14): add gui for todo

---
+++ todos_gui.py
@@ -0,0 +1,12 @@
+import functions
+import FreeSimpleGUI as sg
+
+sg.theme("System Default 1")
+
+label = sg.Text("Type in a to-do")
+input_box = sg.InputText(tooltip="Enter a to-do")
+add_button = sg.Button("Add")
+
+window = sg.Window("App", layout=[[label], [input_box, add_button]])
+window.read()
+window.close()
