f245223fc21b017ad0551404f4b2e1b7ebc73d29
hadventure <halikhacheva@gmail.com>
2024-08-30 15:18:38 +0300

    feat(day 14): add functions module

--- todos.py
+++ todos.py
@@ -1,25 +1,7 @@
+from functions import get_todos, write_todos
+# import functions
+
 user_prompt = f"Please enter a todo: \n"
-
-
-def get_todos(filepath="todos.txt"):
-    """
-    Read a text file and return the list of.
-    """
-    with open(filepath, "a+") as file_local:
-        file_local.seek(0)
-        todos_local = file_local.readlines()
-    return todos_local
-
-
-def write_todos(todos_arg, filepath="todos.txt"):
-    """
-    Read a text file and return the list of.
-    :param todos_arg:
-    :param filepath:
-    :return:
-    """
-    with open(filepath, "w") as file:
-        file.writelines(todos_arg)
 
 
 while True:
