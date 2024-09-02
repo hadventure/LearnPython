f39793c6b4e311037ce8ecb677027ed7e942deca
hadventure <halikhacheva@gmail.com>
2024-09-02 15:15:31 +0300

    feat(day 17): add zip function

--- compressor.py
+++ compressor.py
@@ -1,22 +1,33 @@
 import FreeSimpleGUI as sg
+from zip_creator import make_archive
 
 sg.theme("System Default 1")
 
 label_src = sg.Text("Select files to compress")
 input_src = sg.Input()
-button_src = sg.FilesBrowse("Choose")
+button_src = sg.FilesBrowse("Choose", key="files")
 
 label_dest = sg.Text("Select destination folder")
 input_dest = sg.Input()
-button_dest = sg.FolderBrowse("Choose")
+button_dest = sg.FolderBrowse("Choose", key="folder")
 
 button_compress = sg.Button("Compress")
+
+output_label = sg.Text(key="output", text_color="green")
 
 window = sg.Window("Compressor", layout=[
     [label_src, input_src, button_src],
     [label_dest, input_dest, button_dest],
-    [button_compress]
+    [button_compress, output_label]
 ])
+
+while True:
+    event, values = window.read()
+    print(event, values)
+    filepaths = values["files"].split(";")
+    folder = values["folder"]
+    make_archive(filepaths, folder)
+    window["output"].update("Compression completed.")
 
 window.read()
 window.close()
---
+++ zip_creator.py
@@ -0,0 +1,10 @@
+import zipfile
+import pathlib
+
+def make_archive(filepaths, destination_dir):
+    dest_path = pathlib.Path(destination_dir, "compressed.zip")
+
+    with zipfile.ZipFile(dest_path, "w") as archive:
+        for filepath in filepaths:
+            filepath = pathlib.Path(filepath)
+            archive.write(filepath, arcname=filepath.name)
